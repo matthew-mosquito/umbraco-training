31f3390f6b521c01c227bb9ce281b46c4891673b
matthew-mosquito <matthew@mosquitodigital.co.uk>
2019-05-15 12:12:23 +0100

    Added A sidebar using RenderSection and Partial Views

--- UmbracoTutorial/App_Data/Models/models.generated.cs
+++ UmbracoTutorial/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "5a7afabfbe5a1f99")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.5")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
